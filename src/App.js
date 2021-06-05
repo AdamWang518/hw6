7114832f076339c4bd9d24067c5fb639ab1df519
AdamWang518 <dodoga518@gmail.com>
2021-06-05 19:45:23 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
-const useStyles = makeStyles((theme) => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200,
-  },
-}));
+
 export default function DatePickers() {
   const [selectedDate, setSelectedDate] = React.useState(new Date());
   let year = selectedDate.getFullYear() - 1911;
@@ -33,7 +22,7 @@ export default function DatePickers() {
           format={'民國' + year + '年'+'-MM月-dd號'}
           margin='normal'
           id='date-picker-inline'
-          label='阿公開店日選擇'
+          label='阿公店開店日選擇'
           value={selectedDate}
           onChange={handleDateChange}
           KeyboardButtonProps={{
